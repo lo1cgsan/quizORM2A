c75b44a0e976a81097f991c42bcc963689bcd078
lo1cgsan <lo1cgsan@gmail.com>
2020-06-03 10:44:13 +0200

    modele i dane początkowe

--- quiz-orm/main.py
+++ quiz-orm/main.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 # quiz-orm/main.py
 
-from app import app
+import os
+from app import app, baza
+from models import *
 from views import *
+from dane import *
+
 
 if __name__ == '__main__':
+    if not os.path.exists(app.config['DATABASE']):
+        baza.create_tables((Pytanie, Odpowiedz))
+        dodaj_pytania(pobierz_dane('pytania.csv'))
+
     app.run(debug=True)
